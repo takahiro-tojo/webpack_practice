57b98fed43ed826608a91e6607c0dbd54600b613
daijin <69111234+takahiro-tojo@users.noreply.github.com>
2020-08-14 21:06:01 +0900

    Get pug

--- webpack.config.js
+++ webpack.config.js
@@ -36,14 +36,33 @@ module.exports = {
               },
             ],
           },
+          {
+            test: /\.pug/,
+            use: [
+              {
+                loader: 'html-loader',
+              },
+              {
+                loader: 'pug-html-loader',
+                options: {
+                  pretty: true,
+                },
+              },
+            ],
+          },
         ],
       },
       plugins: [
         new MiniCssExtractPlugin({
-          filename: './css/main.css',
+          filename: './stylesheets/main.css',
         }),
         new HtmlWebpackPlugin({
-          template: './src/templates/index.html',
+          template: './src/templates/index.pug',
+          filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+          template: './src/templates/access.pug',
+          filename: 'access.html',
         }),
         new CleanWebpackPlugin(),
       ],
