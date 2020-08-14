ac4068bfa7cca8fe7d51f0175e3fb40ab7faa832
daijin <69111234+takahiro-tojo@users.noreply.github.com>
2020-08-15 02:25:12 +0900

    Set sass

--- webpack.config.js
+++ webpack.config.js
@@ -14,14 +14,17 @@ module.exports = {
     module: {
         rules: [
           {
-            test: /\.css/,
+            test: /\.(css|scss|sass)$/,
             use: [
               {
                 loader: MiniCssExtractPlugin.loader,
               },
               {
                 loader: 'css-loader',
-              }
+              },
+              {
+                loader: 'sass-loader',
+              },
             ],
           },
           {
@@ -63,6 +66,10 @@ module.exports = {
         new HtmlWebpackPlugin({
           template: './src/templates/access.pug',
           filename: 'access.html',
+        }),
+        new HtmlWebpackPlugin({
+          template: './src/templates/members/taro.pug',
+          filename: 'members/taro.html',
         }),
         new CleanWebpackPlugin(),
       ],
